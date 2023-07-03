044f3ef27e818b9f1c82fad58a0636558597be46
vendrell46 <vendrell_46@hotmail.com>
2023-07-03 17:35:17 +0200

    quote

--- src/pages/postMortemReports/PostMortemReports.jsx
+++ src/pages/postMortemReports/PostMortemReports.jsx
@@ -34,7 +34,7 @@ const PostMortemReports = () => {
               <div className="w-full md:w-2/3">
                 <ul className="list-disc ml-6 mt-4 font-normal text-white">
                   <Report
-                    label="Orion Protocol’s Reentrancy Exploit"
+                    label="Orion Protocol`s Reentrancy Exploit"
                     href="https://quillaudits.medium.com/decoding-orion-protocols-reentrancy-exploit-quillaudits-396de59449f7?source=user_profile"
                   />
                   <Report
@@ -42,15 +42,15 @@ const PostMortemReports = () => {
                     href="https://quillaudits.medium.com/decoding-dforce-protocol-read-only-reentrancy-exploit-quillaudits-c012aed4d666?source=user_profile"
                   />
                   <Report
-                    label="SwapX Protocol’s $1 Million Exploit"
+                    label="SwapX Protocol`s $1 Million Exploit"
                     href="https://quillaudits.medium.com/decoding-swapx-1-million-exploit-quillaudits-502c5e7a542c?source=user_profile"
                   />
                   <Report
-                    label="DKP Token‘s Price Manipulation Exploit"
+                    label="DKP Token`s Price Manipulation Exploit"
                     href="https://quillaudits.medium.com/decoding-dkp-token-s-80k-exploit-quillaudits-45c67df973d6?source=user_profile"
                   />
                   <Report
-                    label="Euler Finance’s $197 Million Exploit"
+                    label="Euler Finance`s $197 Million Exploit"
                     href="https://quillaudits.medium.com/decoding-euler-finances-197-million-exploit-quillaudits-c70fed910d2c?source=user_profile"
                   />
                   <Report
@@ -58,15 +58,15 @@ const PostMortemReports = () => {
                     href="https://quillaudits.medium.com/decoding-kokomo-finance-4-million-rug-pull-quillaudits-d5d027522bf3?source=user_profile"
                   />
                   <Report
-                    label="SafeMoon Protocol’s $8.9 million Exploit"
+                    label="SafeMoon Protocol`s $8.9 million Exploit"
                     href="https://quillaudits.medium.com/decoding-safemoon-protocols-8-9-million-exploit-quillaudits-a5b11417f0b9?source=user_profile"
                   />
                   <Report
-                    label="AllBridge’s $570K Flash Loan Exploit"
+                    label="AllBridge`s $570K Flash Loan Exploit"
                     href="https://quillaudits.medium.com/decoding-allbridge-570k-flash-loan-exploit-quillaudits-8da8dccd729d?source=user_profile"
                   />
                   <Report
-                    label="Sentiment Protocol’s $1 Million Exploit"
+                    label="Sentiment Protocol`s $1 Million Exploit"
                     href="https://quillaudits.medium.com/decoding-sentiment-protocols-1-million-exploit-quillaudits-f36bee77d376?source=user_profile"
                   />
                   <Report
@@ -78,7 +78,7 @@ const PostMortemReports = () => {
                     href="https://quillaudits.medium.com/decoding-beatgenai-bgn-flash-loan-exploit-quillaudits-cfa1823f12c7?source=user_profile"
                   />
                   <Report
-                    label="Ovix Protocol’s $2 Million Exploit"
+                    label="Ovix Protocol`s $2 Million Exploit"
                     href="https://quillaudits.medium.com/decoding-ovix-protocols-2-million-exploit-quillaudits-92befc250e7c?source=user_profile"
                   />
                   <Report
@@ -90,11 +90,11 @@ const PostMortemReports = () => {
                     href="https://quillaudits.medium.com/decoding-swaprum-finance-3-million-rug-pull-quillaudits-2c6f9527b589?source=user_profile"
                   />
                   <Report
-                    label="Jimbo’s Protocol $7.5M Exploit"
+                    label="Jimbo`s Protocol $7.5M Exploit"
                     href="https://quillaudits.medium.com/decoding-jimbos-protocol-7-5m-exploit-quillaudits-772ad1db6c07?source=user_profile"
                   />
                   <Report
-                    label="USEA Token’s $1.1 Million Rug Pull"
+                    label="USEA Token`s $1.1 Million Rug Pull"
                     href="https://quillaudits.medium.com/decoding-usea-tokens-1-1-million-rug-pull-quillaudits-75f049f68e63?source=user_profile"
                   />
                 </ul>
